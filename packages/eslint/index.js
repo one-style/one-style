b1c920699d53f071667777746f5e4531c4c559a7
lidonglin <lidonglin@songxiaocai.com>
2021-05-10 10:43:04 +0800

    feat(@one-style/eslint): plugin:jest/recommended

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,34 +1,28 @@
 exports.getEslint = ({
-    useReact = true,
-    useTs = true,
-    ignorePatterns = [],
-    rules = {},
-    useJest = true,
-} = {
-    
-}) => {
-    return {
-      extends: [
-        useTs && 'plugin:@typescript-eslint/eslint-recommended',
-        'standard',
-        useReact && 'standard-react',
-        'plugin:prettier/recommended',
-        useReact && 'react-app',
-        useJest && 'plugin:jest/all'
-      ].filter(Boolean),
-      parserOptions: {
-        ecmaFeatures: { legacyDecorators: true, jsx: true }
-      },
-      rules: {
-        'no-const-assign': 'error',
-        'react/prop-types': 0,
-        ...rules
-      },
-      ignorePatterns: ['node_modules', ...ignorePatterns],
-      parser: useTs ? '@typescript-eslint/parser' : 'babel-eslint',
-      env: useJest ? {
-        'jest/globals': true,
-      } : {},
-    }
+  useReact = true,
+  useTs = true,
+  ignorePatterns = [],
+  rules = {},
+  useJest = true,
+} = {}) => {
+  return {
+    extends: [
+      useTs && 'plugin:@typescript-eslint/eslint-recommended',
+      'standard',
+      useReact && 'standard-react',
+      'plugin:prettier/recommended',
+      useReact && 'react-app',
+      useJest && 'plugin:jest/recommended',
+    ].filter(Boolean),
+    parserOptions: {
+      ecmaFeatures: { legacyDecorators: true, jsx: true },
+    },
+    rules: {
+      'no-const-assign': 'error',
+      'react/prop-types': 0,
+      ...rules,
+    },
+    ignorePatterns: ['node_modules', ...ignorePatterns],
+    parser: useTs ? '@typescript-eslint/parser' : 'babel-eslint',
   }
-  
+}
